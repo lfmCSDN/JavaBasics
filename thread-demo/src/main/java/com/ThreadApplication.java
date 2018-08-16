f92d0e5d4f35560ade50e8db2d131bb1ac130839
lfmCSDN <lfm940624@163.com>
2018-08-16 23:41:59 +0800

    多线程相关demo

---
+++ thread-demo/src/main/java/com/ThreadApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * create by Lfm
+ * on 2018-08-16 21:39
+ */
+@SpringBootApplication
+public class ThreadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadApplication.class,args);
+    }
+}
---
+++ thread-demo/src/main/java/com/controller/ThreadController.java
@@ -0,0 +1,8 @@
+package com.controller;
+
+/**
+ * create by Lfm
+ * on 2018-08-16 21:45
+ */
+public class ThreadController {
+}
